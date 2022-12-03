f07c6ae50d78f082b108c4c9b090dbd515857c20
0214jj <jeeyoun0214@gmail.com>
2022-12-04 00:06:07 +0900

    [add] 녹음 뷰 구성

--- front/App.js
+++ front/App.js
@@ -1,6 +1,7 @@
-import React, {useState} from 'react';
-import {Pressable, SafeAreaView, StyleSheet, Text} from 'react-native';
-
+import React, {useState, useEffect} from 'react';
+import {Pressable, SafeAreaView, StyleSheet, Text, ImageBackground, TouchableOpacity} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import axios from 'axios';
 
 const App = () => {
 
@@ -26,21 +27,33 @@ const App = () => {
       };
   return (
     <SafeAreaView style={styles.container}>
-        <Text style={styles.appTitle}>우리두리</Text>
-        <Text style={styles.textTitle}>버튼을 누르고 녹음을 시작해주세요!</Text>
-      {getButton(0)}
+        <ImageBackground source ={require("./images/bgimage.jpeg")} style={styles.bgImage} >
+        <Text style={styles.textBox}>하지만 그들은 한 가지 비밀을 공유하지는 않았답니다.
+        {"\n"} 그 비밀은 바로 "백설공주가 마녀"라는 사실이죠.</Text>
+        <Text style={styles.textTitle}>다음 문장을 말해줘! </Text> 
+        <TouchableOpacity style = {styles.recordCircle}>
+          <Text style={styles.recordButton}>
+            <Icon name="record-circle-outline" size={60} color="#ff0000"/>
+          </Text>
+        </TouchableOpacity>
+      </ImageBackground>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  bgImage: {
+    flex:1,
+    width: '100%',
+    height: '100%',
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor:"#ffe700",
   },
-  appTitle: {
+  /*appTitle: {
     color: '#fff',
     fontSize: 36,
     marginTop: 30,
@@ -48,11 +61,26 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     textAlign: 'center',
     backgroundColor:'#ffe700',
+  },*/
+  textBox: {
+    textAlign: 'center',
+    fontSize: 28,
+    fontWeight: '600',
+    backgroundColor: '#fff',
+    marginTop: 110,
+    marginBottom: 30,
+    marginLeft: 30,
+    marginRight: 30,
+    borderRadius: 20,
   },
   textTitle:{
     textAlign: 'center',
-    marginVertical: 8,
+    fontSize: 25,
+    marginVertical: 15,
+    fontWeight: 'bold',
+
   },
+  /*
   buttonContainer: {
     justifyContent: 'center',
     alignItems: 'center',
@@ -60,6 +88,9 @@ const styles = StyleSheet.create({
     height: 50,
     borderRadius: 30,
     marginBottom: 15,
+  },*/
+  recordButton: {
+    alignSelf: 'center',
   },
 });
 
